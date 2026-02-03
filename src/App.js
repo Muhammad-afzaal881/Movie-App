51426429bf174dec0dd3d3259f1e7d933e4f816a
abbas-tr <abbas.a@turing.com>
2026-02-03 11:48:18 +0500

    everythong is upto date

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Navbar from "./Navbar";
-import Card from './Card'
-import About from './About'
+import Navbar from "./components/Navbar";
+import Card from './components/Card'
+import About from './components/About'
 import {  Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
    <>
    <Navbar/>
+   
    <Routes>
+    <Route path="/" element={<Card />} />
         <Route path="/about" element={<About/>} />
         <Route path="/Home" element={<Card />} />
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,15 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="1">Navbar</a>
+    
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="Home">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
        <li className="nav-item">
   <Link  className="nav-link active"to="/about">About us</Link>
