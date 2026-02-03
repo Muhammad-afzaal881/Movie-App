334c3fb2d1f1b4cc76e66f3c4fbcb9f31ec9b381
abbas-tr <abbas.a@turing.com>
2026-02-03 11:51:36 +0500

    everything uptdate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href="1">Navbar</a>
+    <h1>Navbar</h1>
     
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
